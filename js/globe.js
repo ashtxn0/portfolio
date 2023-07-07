47b484b88e33b35a9e32284b72199cccc858d79d
Ashton Areia <ashtonareia@outlook.com>
2023-07-07 18:51:16 -0400

    fix globe on mobile

--- js/globe.js
+++ js/globe.js
@@ -1,22 +1,20 @@
 const scene = new THREE.Scene();
 
 
-let camera={};
-console.log(innerWidth)
-if (innerWidth<901){
+let camera = {};
+if (innerWidth < 901) {
   camera = new THREE.PerspectiveCamera(90, innerWidth / innerHeight, 0.1, 1000);
-} else{
+} else {
   camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 }
-var canvas = document.getElementById("globe-canvas")
 
-
+var canvas = document.getElementById("globe-canvas");
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true
 });
 renderer.setSize(innerWidth, innerHeight);
-renderer.setClearColor(0x000000,0.0);
+renderer.setClearColor(0x000000, 0.0);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
@@ -30,92 +28,120 @@ Promise.all([
     const globeShader = new THREE.ShaderMaterial({
       vertexShader: vertexShaderSource,
       fragmentShader: fragmentShaderSource,
-      uniforms:{
+      uniforms: {
         globeTexture: {
-            value: new THREE.TextureLoader().load("./assets/images/blue-globe-uv2.jpg")
+          value: new THREE.TextureLoader().load("./assets/images/blue-globe-uv2.jpg",
+            // On success
+            function (texture) {
+              console.log("Success!");
+              console.log(texture);
+            },
+
+            // Progress (ignored)
+            undefined,
+
+            // On error
+            function (err) {
+              console.log("Error");
+              console.log(err);
+            })
         }
       }
     });
+
     const atmosphereShader = new THREE.ShaderMaterial({
-        vertexShader: atmosphereVertexShaderSource,
-        fragmentShader: atmosphereFragmentShaderSource,
-        blending: THREE.AdditiveBlending,
-        side: THREE.BackSide
-      });
+      vertexShader: atmosphereVertexShaderSource,
+      fragmentShader: atmosphereFragmentShaderSource,
+      blending: THREE.AdditiveBlending,
+      side: THREE.BackSide
+    });
 
     const sphere = new THREE.Mesh(
       new THREE.SphereGeometry(4, 50, 50),
       globeShader
     );
 
-
-    //create atmosphere
     const atmosphere = new THREE.Mesh(
-        new THREE.SphereGeometry(4, 50, 50),
-        atmosphereShader
-      );
-    atmosphere.scale.set(1.1,1.1,1.1)
+      new THREE.SphereGeometry(4, 50, 50),
+      atmosphereShader
+    );
+    atmosphere.scale.set(1.1, 1.1, 1.1);
     scene.add(atmosphere);
 
-    const group = new THREE.Group()
-    group.add(sphere)
-    scene.add(group)
+    const group = new THREE.Group();
+    group.add(sphere);
+    scene.add(group);
 
     camera.position.z = 10;
 
     const mouse = {
       x: undefined,
       y: undefined
-    }
+    };
 
     function animate() {
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
-      sphere.rotation.y += 0.003
-      gsap.to(group.rotation,{
-        x: mouse.y * 0.3,
-        y: mouse.x * 0.5,
-        duration: 2
-      })
+      sphere.rotation.y += 0.003;
+      if (!isMobileDevice) {
+        gsap.to(group.rotation, {
+          x: mouse.y * 0.3,
+          y: mouse.x * 0.5,
+          duration: 2
+        });
+      }
     }
-
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobileDevice = /mobile|android|iphone|ipad|iemobile|wpdesktop|windows phone|blackberry/i.test(userAgent);
     animate();
 
 
-    
-    addEventListener("mousemove", (event)=>{
-      mouse.x = (event.clientX / innerWidth)*2-1;
-      mouse.y = (event.clientY / innerHeight)*2+1
-    })
+    function handleMouseMove(event) {
+      mouse.x = (event.clientX / innerWidth) * 2 - 1;
+      mouse.y = (event.clientY / innerHeight) * 2 + 1;
+    }
+
+    addEventListener("mousemove", handleMouseMove);
+
+    // Trigger the mousemove event on page load
+    const syntheticEvent = new MouseEvent("mousemove", {
+      clientX: window.innerWidth / 100,
+      clientY: window.innerHeight / 100
+    });
+    handleMouseMove(syntheticEvent);
+
+    if (isMobileDevice) {
+      removeEventListener("mousemove", handleMouseMove);
+    }
   })
   .catch(error => {
     console.error('Error loading shaders', error);
   });
 
-  function render() {
-    if (resize(renderer)) {
-        camera.aspect = canvas.clientWidth / canvas.clientHeight;
-        camera.updateProjectionMatrix();
-    }
-    renderer.render(scene, camera);
-    requestAnimationFrame(render);
+function render() {
+  if (resize(renderer)) {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
 }
 
 function resize(renderer) {
-    const canvas = renderer.domElement;
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
-    const needResize = canvas.width !== width || canvas.height !== height;
-    if (needResize) {
-        renderer.setSize(width, height, false);
-    }
-    return needResize;
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
 }
 
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
-window.addEventListener( 'resize', function() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight )
-})
+render();
